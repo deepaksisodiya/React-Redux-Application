11cb2a510994d19264d56dc9ff365b938bcf16ad
deepak sisodiya <deepak.sisodiya@courgetteconcepts.com>
2016-06-05 14:30:24 +0530

    added axios http client

--- index.js
+++ index.js
@@ -5,8 +5,20 @@
 
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
 export default class Hello extends Component {
+
+  constructor(props) {
+    super(props);
+    axios.get('http://localhost:3000/users')
+      .then(function (response) {
+        console.log(response);
+      })
+      .catch(function (response) {
+        console.log(response);
+      });
+  }
 
   render() {
     return(
