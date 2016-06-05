e6122d26016af3e08424c62a8d156b0077fe6aa9
deepak sisodiya <deepak.sisodiya@courgetteconcepts.com>
2016-06-05 13:49:48 +0530

    reactjs component working with ES6

--- index.js
+++ index.js
@@ -16,3 +16,18 @@ const [a, b, ...rest] = [1,2,3,4,5];
 console.log(a);
 console.log(b);
 console.log(...rest);
+
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class Hello extends Component {
+
+  render() {
+    return(
+      <h1>Hello React</h1>
+    )
+  }
+
+}
+
+ReactDOM.render(<Hello />, document.getElementById('container'));
