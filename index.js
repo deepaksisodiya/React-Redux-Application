e2f588013a36df80d26669f9150321b724e3a036
deepak sisodiya <deepak.sisodiya@courgetteconcepts.com>
2016-06-06 19:58:46 +0530

    changes

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router'
 
 import CreateEditUser from './src/components/CreateEditUser';
-import showUsers from './src/components/ShowUsers';
+import ShowUsers from './src/components/ShowUsers';
 
 ReactDOM.render((
   <Router history={hashHistory}>
-    <Route path="/" component={showUsers}></Route>
+    <Route path="/" component={ShowUsers}></Route>
     <Route path="/create" component={CreateEditUser}></Route>
     <Route path="/edit/:userId" component={CreateEditUser}></Route>
   </Router>
